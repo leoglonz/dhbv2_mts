8e82e6ebd64f60d73620b5e0960c5448820c26e4
Leo Lonzarich <lgl5139@psu.edu>
2025-12-08 00:43:49 -0600

    update init

--- src/dhbv2/__init__.py
+++ src/dhbv2/__init__.py
@@ -1,10 +1,6 @@
-from dhbv2._version import __version__
 from dhbv2.pet import calc_hourly_hargreaves_pet
 from dhbv2.bmi import DeltaModelBmi
 from dhbv2.bmi_mts import DeltaModelBmi as MtsDeltaModelBmi
-
-# In case setuptools scm says version is 0.0.0
-assert not __version__.startswith('0.0.0')
 
 __all__ = [
     "calc_hourly_hargreaves_pet",
